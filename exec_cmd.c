02be6e0ac5af5706f0005f8da5d542d3f05464ac
Lionnel7 <pierrerichan@gmail.com>
2025-04-23 21:55:00 +0200

    Implement basic command execution using fork and exec

---
+++ exec_cmd.c
@@ -0,0 +1,92 @@
+#include "shell.h"
+
+/**
+ * get_cmd - Finds the full path of a command
+ *
+ * @arg: The command name
+ *
+ * Return: Full path of the command or NULL if not found
+ */
+char *get_cmd(char *arg)
+{
+	char *path, *tmp_path, *token, full_path[1024];
+	struct stat st;
+
+	if (strchr(arg, '/'))
+	{
+		if (stat(arg, &st) == 0)
+			return (strdup(arg));
+
+		return (NULL);
+	}
+	path = _getenv("PATH");
+
+	if (!path)
+		return (NULL);
+
+	tmp_path = strdup(path);
+	token = strtok(tmp_path, ":");
+
+	while (token)
+	{
+		sprintf(full_path, "%s/%s", token, arg);
+		if (stat(full_path, &st) == 0 && access(full_path, X_OK) == 0)
+		{
+			free(tmp_path);
+			return (strdup(full_path));
+		}
+		token = strtok(NULL, ":");
+	}
+	free(tmp_path);
+	return (NULL);
+}
+
+/**
+ * exec_command - Executes an external command
+ *
+ * @context: Pointer to the execution context containing data and state
+ *
+ * Return: void
+ */
+void exec_command(exec_context_t *context)
+{
+	char *command = get_cmd(context->args[0]);
+	pid_t pid;
+	int status;
+
+	context->exec_count += 1;
+	if (!command)
+	{
+		print_error(context, 127);
+		return;
+	}
+	if (access(command, X_OK) == -1)
+	{
+		print_error(context, 126);
+		free(command);
+		return;
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		fprintf(stderr, "Error: Fork failed\nExit...\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(command, context->args, environ) == -1)
+		{
+			fprintf(stderr, "Error: Command execution failed\nExit...\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
+			context->exit_code = 2;
+		else
+			context->exit_code = WEXITSTATUS(status);
+	}
+	free(command);
+}
